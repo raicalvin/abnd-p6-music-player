833664ea978095361298171176f4d9b1d2efa53d
Calvin Rai <raicalvin@gmail.com>
2018-06-08 21:23:43 -0700

    Added private Song.java member variables.

--- app/src/main/java/com/example/gomezmusicplayer/Song.java
+++ app/src/main/java/com/example/gomezmusicplayer/Song.java
@@ -1,4 +1,14 @@
 package com.example.gomezmusicplayer;
 
 public class Song {
+
+    /** Song title */
+    private String mSongTitle;
+
+    /** Song artist */
+    private String mSongArtist;
+
+    /** Resource ID for album cover image */
+    private int mImageResourceID;
+
 }
