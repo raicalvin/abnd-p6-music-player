745a11caa094c9d30ad452b6516da0f56dba931a
Calvin Rai <raicalvin@gmail.com>
2018-06-09 18:10:10 -0700

    Populated SongAdapter with default constructor.

--- app/src/main/java/com/example/gomezmusicplayer/SongAdapter.java
+++ app/src/main/java/com/example/gomezmusicplayer/SongAdapter.java
@@ -1,6 +1,18 @@
 package com.example.gomezmusicplayer;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.widget.ArrayAdapter;
 
 public class SongAdapter extends ArrayAdapter {
+
+    /**
+     * Constructor
+     *
+     * @param context  The current context.
+     * @param resource The resource ID for a layout file containing a TextView to use when
+     */
+    public SongAdapter(@NonNull Context context, ArrayList<Song> songs) {
+        super(context, 0, songs);
+    }
 }
