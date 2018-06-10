34707fcd9bb8fb9e5e9d709a8684ce2557d2b2c4
Calvin Rai <raicalvin@gmail.com>
2018-06-09 18:46:11 -0700

    Populated SongAdapter to MainActivity.

--- app/src/main/java/com/example/gomezmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/gomezmusicplayer/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.gomezmusicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Song> songs = new ArrayList<>();
+
+        songs.add(new Song("Back To You", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Stars Dance", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Come And Get It", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Bad Liar", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Birthday", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Kill 'Em With Kindness", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Wolves", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Hands To Myself", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Same Old Love", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("The Heart Wants What It Wants", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Slow Down", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Me And The Rhythm", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("A Year Without Rain", "Selena Gomez", R.drawable.cover_stars_dance));
+        songs.add(new Song("Love Will Remember", "Selena Gomez", R.drawable.cover_stars_dance));
+
+        SongAdapter adapter = new SongAdapter(this, songs);
+
+        ListView listView = findViewById(R.id.list_songs);
+
+        listView.setAdapter(adapter);
+
+
     }
 }
--- app/src/main/java/com/example/gomezmusicplayer/SongAdapter.java
+++ app/src/main/java/com/example/gomezmusicplayer/SongAdapter.java
@@ -2,11 +2,22 @@ package com.example.gomezmusicplayer;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class SongAdapter extends ArrayAdapter {
+
+    private Context mContext;
+    private List<Song> songsList = new ArrayList<>();
 
     /**
      * Constructor
@@ -15,5 +26,28 @@ public class SongAdapter extends ArrayAdapter {
      */
     public SongAdapter(@NonNull Context context, ArrayList<Song> songs) {
         super(context, 0, songs);
+        mContext = context;
+        songsList = songs;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        Song song = (Song) getItem(position);
+
+        if(convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.song_item,parent,false);
+        }
+
+        TextView songTitleTextView = (TextView) convertView.findViewById(R.id.song_library_text_view);
+        TextView artistTitleTextView = (TextView) convertView.findViewById(R.id.artist_library_text_view);
+        ImageView albumeCoverImageView = (ImageView) convertView.findViewById(R.id.library_image);
+
+        songTitleTextView.setText(song.getSongTitle());
+        artistTitleTextView.setText(song.getSongArtist());
+        albumeCoverImageView.setImageResource(song.getImageResourceID());
+
+        return convertView;
     }
 }
