9eec2467df337307162b60c4324ac099dc77e424
Calvin Rai <raicalvin@gmail.com>
2018-06-09 18:21:39 -0700

    Updated song_item.xml layout.

--- app/src/main/java/com/example/gomezmusicplayer/SongAdapter.java
+++ app/src/main/java/com/example/gomezmusicplayer/SongAdapter.java
@@ -3,6 +3,8 @@ package com.example.gomezmusicplayer;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+
 
 public class SongAdapter extends ArrayAdapter {
 
@@ -10,7 +12,6 @@ public class SongAdapter extends ArrayAdapter {
      * Constructor
      *
      * @param context  The current context.
-     * @param resource The resource ID for a layout file containing a TextView to use when
      */
     public SongAdapter(@NonNull Context context, ArrayList<Song> songs) {
         super(context, 0, songs);
