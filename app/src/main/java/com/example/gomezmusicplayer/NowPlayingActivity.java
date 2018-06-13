bd40cad7f5a18ddf8bb50ea66fca530478cc2e44
Calvin Rai <raicalvin@gmail.com>
2018-06-12 21:25:05 -0700

    Added final activity intent for Shopping Item Activity.

--- app/src/main/java/com/example/gomezmusicplayer/NowPlayingActivity.java
+++ app/src/main/java/com/example/gomezmusicplayer/NowPlayingActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class NowPlayingActivity extends AppCompatActivity {
@@ -15,8 +17,8 @@ public class NowPlayingActivity extends AppCompatActivity {
 
         Intent receivedIntent = getIntent();
 
-        String incomingSongName = receivedIntent.getStringExtra("SONG_TITLE");
-        String incomingArtistName = receivedIntent.getStringExtra("ARTIST_NAME");
+        final String incomingSongName = receivedIntent.getStringExtra("SONG_TITLE");
+        final String incomingArtistName = receivedIntent.getStringExtra("ARTIST_NAME");
 
         Log.i("What Up", "Seeeeleeeeeeeena: " + incomingSongName);
 
@@ -25,6 +27,18 @@ public class NowPlayingActivity extends AppCompatActivity {
 
         TextView artistNameTextView = (TextView) findViewById(R.id.artist_name_text_view);
         artistNameTextView.setText(incomingArtistName);
+
+        ImageView shoppingBagTextView = (ImageView) findViewById(R.id.icon_shopping);
+
+        shoppingBagTextView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent openStoreListingIntent = new Intent(NowPlayingActivity.this, StoreListingActivity.class);
+                openStoreListingIntent.putExtra("SONG_TITLE", incomingSongName);
+                openStoreListingIntent.putExtra("ARTIST_NAME", incomingArtistName);
+                startActivity(openStoreListingIntent);
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/gomezmusicplayer/StoreListingActivity.java
+++ app/src/main/java/com/example/gomezmusicplayer/StoreListingActivity.java
@@ -1,13 +1,34 @@
 package com.example.gomezmusicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class StoreListingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_store_listing);
+        setContentView(R.layout.store_listing_item);
+
+        Intent receivedIntent = getIntent();
+
+        String incomingSongName = receivedIntent.getStringExtra("SONG_TITLE");
+        String incomingArtistName = receivedIntent.getStringExtra("ARTIST_NAME");
+
+        TextView songTitleTextView = (TextView) findViewById(R.id.store_listing_song_title);
+        TextView artistNameTextView = (TextView) findViewById(R.id.store_listing_artist_name);
+
+        Log.i("StoreListingActivity", "store listing song name " + incomingSongName);
+        Log.i("StoreListingActivity", "store listing artist name " + incomingArtistName);
+
+
+        songTitleTextView.setText(incomingSongName);
+        artistNameTextView.setText(incomingArtistName);
+
+
+
     }
 }
